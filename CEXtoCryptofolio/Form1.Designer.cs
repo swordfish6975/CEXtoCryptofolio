5ba31105779a5e938dfaf3e2e48718c061e28df9
Steven J. Cole <steven.j.cole@gmail.com>
2014-01-18 23:28:37 +1000

    first commit

---
+++ CEXtoCryptofolio/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace CEXtoCryptofolio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbxUUID = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbxKey = new System.Windows.Forms.TextBox();
+            this.tbxSecret = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Visit:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(65, 26);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(75, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "cryptofolio.info";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2.  Use the drop downs to add one only of:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "LTC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "NMC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "IXC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "DVC";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(82, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "BTC";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(214, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "3. From the link in the top left get your UUID";
+            // 
+            // tbxUUID
+            // 
+            this.tbxUUID.Location = new System.Drawing.Point(66, 170);
+            this.tbxUUID.Name = "tbxUUID";
+            this.tbxUUID.Size = new System.Drawing.Size(218, 20);
+            this.tbxUUID.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 173);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "UUID:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(14, 257);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(297, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "5. Login to your CEX.IO account under profle make a API key";
+            // 
+            // tbxKey
+            // 
+            this.tbxKey.Location = new System.Drawing.Point(68, 294);
+            this.tbxKey.Name = "tbxKey";
+            this.tbxKey.Size = new System.Drawing.Size(218, 20);
+            this.tbxKey.TabIndex = 13;
+            // 
+            // tbxSecret
+            // 
+            this.tbxSecret.Location = new System.Drawing.Point(68, 321);
+            this.tbxSecret.Name = "tbxSecret";
+            this.tbxSecret.Size = new System.Drawing.Size(218, 20);
+            this.tbxSecret.TabIndex = 14;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(37, 297);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Key:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(25, 325);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "Secret:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(7, 225);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "Username:";
+            // 
+            // tbxUserName
+            // 
+            this.tbxUserName.Location = new System.Drawing.Point(68, 221);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(218, 20);
+            this.tbxUserName.TabIndex = 19;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(13, 203);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(154, 13);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "4. Enter your CEX.IO username";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label15);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxUserName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbxSecret);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbxKey);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.tbxUUID);
+            this.groupBox1.Location = new System.Drawing.Point(31, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 389);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setup";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(82, 65);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(30, 13);
+            this.label16.TabIndex = 23;
+            this.label16.Text = "GHS";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(13, 355);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(247, 13);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "6. Click Activate on the key and verify in your email";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 452);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CEXtoCryptofolio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbxUUID;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbxKey;
+        private System.Windows.Forms.TextBox tbxSecret;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbxUserName;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
+
---
+++ CEXtoCryptofolio/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+
+namespace CEXtoCryptofolio
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly Encoding encoding = Encoding.UTF8; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            var temp = CEXtoCryptofolio.Properties.Settings.Default;
+            tbxUUID.Text = temp.UUID;
+            tbxSecret.Text = temp.Secret;
+            tbxKey.Text = temp.Key;
+            tbxUserName.Text = temp.Username;
+
+
+        }
+
+        public  string downloadData()
+        {
+            try
+            {
+                TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+                string nonce = ((int)t.TotalSeconds).ToString();
+
+                var temp = CEXtoCryptofolio.Properties.Settings.Default;
+
+                string key = temp.Key;
+                string secret = temp.Secret;
+                string username = temp.Username;
+
+                string signature = hashIT(secret, nonce + username + key);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("https://cex.io/api/balance/");
+
+
+                //t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+                //nonce = t.TotalSeconds.ToString();
+
+                Uri myUri = new Uri(sb.ToString());
+                // Create a 'HttpWebRequest' object for the specified url. 
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(myUri);
+                myHttpWebRequest.Timeout = 50000;  //  1/2 the default 
+                // Set the user agent as if we were a web browser
+                myHttpWebRequest.UserAgent = @"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.4) Gecko/20060508 Firefox/1.5.0.4";
+
+                myHttpWebRequest.Method = "POST";
+                myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                using (var writer = new StreamWriter(myHttpWebRequest.GetRequestStream()))
+                {
+                    writer.Write("key=" + key + "&signature=" + signature + "&nonce=" + nonce);
+                }
+
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+                var stream = myHttpWebResponse.GetResponseStream();
+                var reader = new StreamReader(stream);
+                var html = reader.ReadToEnd();
+                // Release resources of response object.
+                myHttpWebResponse.Close();
+
+                //html = html.Substring(html.IndexOf("last") + 6);
+                //html = html.Split(',')[0];
+
+                return html;
+            }
+            catch
+            {
+                return "ERROR";
+            }
+
+
+        }
+
+        static string hashIT(string key,string message)
+        {
+            var keyByte = encoding.GetBytes(key);
+            var hmacsha256 = new HMACSHA256(keyByte);
+            var messageBytes = encoding.GetBytes(message);
+            hmacsha256.ComputeHash(messageBytes);
+
+            return ByteToString(hmacsha256.Hash);
+        }
+
+        static string ByteToString(byte[] buff)
+        {
+            string sbinary = "";
+            for (int i = 0; i < buff.Length; i++)
+                sbinary += buff[i].ToString("X2"); /* hex format */
+            return sbinary;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://cryptofolio.info");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var temp = CEXtoCryptofolio.Properties.Settings.Default;
+            temp.UUID = tbxUUID.Text;
+            temp.Secret = tbxSecret.Text;
+            temp.Key = tbxKey.Text;
+            temp.Username = tbxUserName.Text;
+            temp.Save();
+
+
+            string UUID = temp.UUID;
+
+            string data = downloadData();
+           // MessageBox.Show(data);
+
+            dynamic dynamicJson = JsonConvert.DeserializeObject(data);
+            // MessageBox.Show(temp.timestamp.ToString());
+
+            Dictionary<string, string> coins = new Dictionary<string, string>();
+
+            if(dynamicJson.LTC.orders == null)
+                coins.Add("LTC", dynamicJson.LTC.available.ToString());
+            else
+                coins.Add("LTC", (((decimal)dynamicJson.LTC.available) + ((decimal)dynamicJson.LTC.orders)).ToString());
+
+            if (dynamicJson.BTC.orders == null)
+                coins.Add("BTC", dynamicJson.BTC.available.ToString());
+            else
+                coins.Add("BTC", (((decimal)dynamicJson.BTC.available) + ((decimal)dynamicJson.BTC.orders)).ToString());
+
+            if (dynamicJson.NMC.orders == null)
+                coins.Add("NMC", dynamicJson.NMC.available.ToString());
+            else
+                coins.Add("NMC", (((decimal)dynamicJson.NMC.available) + ((decimal)dynamicJson.NMC.orders)).ToString());
+
+            if (dynamicJson.IXC.orders == null)
+                coins.Add("IXC", dynamicJson.IXC.available.ToString());
+            else
+                coins.Add("IXC", (((decimal)dynamicJson.IXC.available) + ((decimal)dynamicJson.IXC.orders)).ToString());
+
+            if (dynamicJson.DVC.orders == null)
+                coins.Add("DVC", dynamicJson.DVC.available.ToString());
+            else
+                coins.Add("DVC", (((decimal)dynamicJson.DVC.available) + ((decimal)dynamicJson.DVC.orders)).ToString());
+
+            if (dynamicJson.GHS.orders == null)
+                coins.Add("GHS", dynamicJson.GHS.available.ToString());
+            else
+                coins.Add("GHS", (((decimal)dynamicJson.GHS.available) + ((decimal)dynamicJson.GHS.orders)).ToString());
+
+
+            foreach (var coin in coins)
+            {
+
+                string URL = "http://cryptofolio.info/api/v1/?@UUID=" + UUID + "&@VALUE=@UpdateQuantity&@CODE=" + coin.Key + "&@Quantity=" + coin.Value;
+                WebRequest webRequest = WebRequest.Create(URL);
+                WebResponse webResp = webRequest.GetResponse();
+                var stream = webResp.GetResponseStream();
+                var reader = new StreamReader(stream);
+                var html = reader.ReadToEnd();
+
+                if (html != "Success")
+                    MessageBox.Show("Failed to upload Coin:" + coin.Key + " Value:" + coin.Value);
+
+                webResp.Close();
+            }
+
+            MessageBox.Show("Done");
+        }    
+    }
+}
